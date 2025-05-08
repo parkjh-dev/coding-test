2cee11e3523c2fd4277630bc7d2e44c196e20021
parkjh-dev <pjh6444@gmail.com>
2025-05-08 11:14:46 +0900

    Delete Kotlin/프로그래머스/0 directory

--- Kotlin/프로그래머스/0/181886. 5명씩/5명씩.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun solution(names: Array<String>): Array<String> {
-        var answer: Array<String> = arrayOf<String>()
-        for(i in names.indices){
-            if((i) % 5 == 0) answer += names[i]
-        }
-        return answer
-    }
-}
